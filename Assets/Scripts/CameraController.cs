257118cd114a672bbb60a1691a157d240da0cd56
ruru112233 <esxwaz1234@gmail.com>
2021-01-09 12:54:05 +0900

    20210109_カメラ切り替え機能の追加

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        // 時計回り
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.RotateAround(target.transform.position, new Vector3(0, 1, 0), 20.0f * Time.deltaTime);
+        }
+
+        // 反時計回り
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.RotateAround(target.transform.position, new Vector3(0, -1, 0), 20.0f * Time.deltaTime);
+        }
+
+    }
+}
